39e48266a0e73990e09deaed668a70615d442ab2
ali-em <ali@localhost.localdomain>
2020-07-24 05:56:12 +0430

    Add creating task group

--- scripts/script.js
+++ scripts/script.js
@@ -3,15 +3,17 @@ const colsContainer = document.getElementById("cols-container");
 const addCol = document.getElementById("add-col");
 
 const actionClasses = ["box", "action", "clickable"];
-let actions = ["Todo", "Doing", "Done"];
+let actions = ["Todo"];
 let lastID = 0;
 const generateTaskId = (id) => `task${id}`;
 const getTaskById = (id) => document.getElementById(generateTaskId(id));
 const getAllActionBoxes = () => document.getElementsByClassName("action-box");
 const remove = (id) => getTaskById(id).remove();
-const changeGroup = (id, dest) =>
-    document.getElementById(dest).append(getTaskById(id));
-
+const changeGroup = (id, dest) => {
+    const task = document.getElementById(id);
+    if (task.parentNode.id == dest) return;
+    document.getElementById(dest).append(task);
+};
 const createAction = (id, actionTitle) => {
     const action = document.createElement("div");
     action.classList.add(...actionClasses);
@@ -20,13 +22,39 @@ const createAction = (id, actionTitle) => {
     return action;
 };
 
+const createGroupColumn = (name) => {
+    const EmptyDiv = document.createElement("div");
+    const innerDiv = document.createElement("div");
+    innerDiv.classList.add("col", "box");
+    const header = document.createElement("h3");
+    header.innerText = name;
+    innerDiv.append(header);
+    const taskCat = document.createElement("div");
+    taskCat.id = name;
+    taskCat.classList.add("task-cat");
+    innerDiv.append(taskCat);
+    EmptyDiv.append(innerDiv);
+    return EmptyDiv;
+};
+
+const addGroup = (name) => {
+    if (actions.includes(name)) return alert("Duplicate name!");
+    actions.push(name);
+    colsContainer.insertBefore(createGroupColumn(name), addCol.parentElement);
+    const actionBoxes = getAllActionBoxes();
+    for (let i = 0, l = actionBoxes.length; i < l; i++) {
+        const id = actionBoxes[i].parentElement.id;
+        actionBoxes[i].appendChild(createAction(id, name));
+    }
+};
+
 const createTask = (title) => {
     const id = lastID++;
     const actionsBox = document.createElement("div");
     actionsBox.classList.add("action-box");
     actionsBox.id = "";
     actions.map((actionTitle) => {
-        actionsBox.append(createAction(id, actionTitle));
+        actionsBox.append(createAction(generateTaskId(id), actionTitle));
     });
 
     const action = document.createElement("div");
@@ -49,4 +77,8 @@ const createTask = (title) => {
 document.getElementById("add-todo").addEventListener("click", () => {
     if (input.value.trim() !== "")
         document.getElementById("Todo").append(createTask(input.value));
+    input.value = "";
+});
+addCol.addEventListener("click", () => {
+    addGroup(prompt("Enter the group name please:"));
 });
